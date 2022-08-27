1d987f9f46a2a757fea669a589f673e8a53ea479
malik-hamza-mehboob-dev <malik.hamza.mehboob.dev@gmail.com>
2022-08-27 07:11:13 +0500

    Heroku Configurations

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
+// Route Handler
 app.get('/',(req,res)=>{
     res.send({hi:'there'});
 });
 
-app.listen(5000);
+// Dynamic Port to listen to for Heroku || localhost:5000
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
